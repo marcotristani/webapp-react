8d912e4d7ccfb7807fd83cb5ea411e2de815b3eb
marcotristani <marco.tristani@hotmail.it>
2026-02-26 14:37:48 +0100

    refactoring: aggiunto logo e slogan header e definito style header

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,10 +1,24 @@
 import { Outlet } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function MainLayout() {
   return (
     <>
-      <header>
-        <h1 className="text-warning">Sono l'header</h1>
+      <header className="bg-dark position-fixed">
+        <div class=" container py-3 d-flex justify-content-around align-items-center">
+          <div className="col">
+            <Link to="/">
+              <img
+                className="logo"
+                src="src/assets/logo_cinescaffale.png"
+                alt="Logo CineScaffale"
+              />
+            </Link>
+          </div>
+          <div className="col">
+            <h1 className="text-light ">Tutto il cinema, a portata di click</h1>
+          </div>
+        </div>
       </header>
       <main>
         <Outlet />
